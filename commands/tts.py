dc527194054046e7b6a067b644722fec20aade76
reacheight <reacheight8@gmail.com>
2017-11-03 23:03:40 +0300

    fixed text to speach function, chenged help command message, updated project requirements file

--- commands/tts.py
+++ commands/tts.py
@@ -1,14 +1,19 @@
 import os
 from gtts import gTTS
 
+import config
 from bot import bot
 
 
 def text_to_speech(message):
-    voice = gTTS(text=' '.join(message.text.split()[1:]), lang='ru')
-    voice.save("voice_file.ogg")
+    if len(message.text.split()) > 1:
+        voice = gTTS(text=' '.join(message.text.split()[1:]), lang='ru')
+        voice.save("voice_file.ogg")
 
-    voice_file = open("voice_file.ogg", "rb")
-    bot.send_voice(message.chat.id, voice_file, reply_to_message_id=message.message_id)
+        if "voice_file.ogg" in os.listdir(os.curdir):
+            voice_file = open("voice_file.ogg", "rb")
+            bot.send_voice(message.chat.id, voice_file, reply_to_message_id=message.message_id)
+            os.remove("voice_file.ogg")
 
-    os.remove("voice_file.ogg")
+    else:
+        bot.reply_to(message, config.tts_empty_query_message, parse_mode="Markdown")
--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
 
-token = os.environ['BOT_TOKEN']
+# token = os.environ['BOT_TOKEN']
+token = '461762429:AAHCByDfxgGYMTbkwCPjfwipSX0pYC0nsAE'
 
 bl_text_file = 'data/bl_data/bl_text_messages'
 help_text_file = 'data/help_data/help_text_message'
 bl_images_locations = 'data/bl_data/images/'
 
-wolfram_appid = os.environ['WOLFRAM_APPID']
+# wolfram_appid = os.environ['WOLFRAM_APPID']
+wolfram_appid = '7PKA2K-Y8ULYG5TAY'
 wolfram_bad_status_message = "Запрос не найдён.\nЕсли ты ввёл его на русском, то попробуй ввести его на английском."
 wolfram_empty_query_message = "Использование: `/wolfram <запрос>` или `/wf <запрос>`"
+
+tts_empty_query_message = "Использование: `/tts <запрос>` или `/wf <запрос>`"
