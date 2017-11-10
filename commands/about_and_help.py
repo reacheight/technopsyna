68d24dbcf0ac4dff263ec06e57c92a2fa3d2052b
reacheight <reacheight8@gmail.com>
2017-11-11 01:28:42 +0300

    another one

--- commands/about_and_help.py
+++ commands/about_and_help.py
@@ -4,7 +4,7 @@ from bot import bot
 
 def my_help(message):
     help_message = open(config.help_text_file, 'r', encoding='utf-8')
-    bot.send_message(message.chat.id, help_message.read().strip(), parse_mode="Markdown", disable_webpage_preview=True)
+    bot.send_message(message.chat.id, help_message.read().strip(), disable_webpage_preview=True, parse_mode="Markdown")
 
     help_message.close()
 
