d4886cb7e45ec709e818dd32f6d69829175770b8
reacheight <reacheight8@gmail.com>
2017-11-17 00:27:47 +0300

    little upgrade help command

--- commands/about_and_help.py
+++ commands/about_and_help.py
@@ -4,7 +4,7 @@ from bot import bot
 
 def my_help(message):
     help_message = open(config.help_text_file, 'r', encoding='utf-8')
-    bot.send_message(message.chat.id, help_message.read().strip(), parse_mode="Markdown")
+    bot.send_message(message.chat.id, help_message.read().strip(), parse_mode="Markdown", disable_web_page_preview=True)
 
     help_message.close()
 
