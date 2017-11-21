c157cd6c64a142f866ddf60ee6f70da910462542
reacheight <reacheight8@gmail.com>
2017-11-21 16:09:26 +0300

    added support of logs

--- techconfbot.py
+++ techconfbot.py
@@ -2,6 +2,7 @@ import re
 
 from commands import bl, about_and_help, wolfram, tts, pidor, deer
 from bot import bot
+from log import log
 
 import config
 
@@ -9,46 +10,55 @@ import config
 @bot.message_handler(commands=['bl'])
 def bl_command(message):
     bl.my_bl(message)
+    log(message)
 
 
 @bot.message_handler(commands=['wf', 'wolfram'])
 def wf_command(message):
     wolfram.wolfram_command(message)
+    log(message)
 
 
 @bot.message_handler(commands=['tts', 'voice'])
 def tts_command(message):
     tts.text_to_speech(message)
+    log(message)
 
 
 @bot.message_handler(commands=['deer_message'])
 def deer_message_command(message):
     deer.get_messages(message)
+    log(message)
 
 
 @bot.message_handler(commands=['pidoreg'])
 def pidoreg_command(message):
     pidor.registration(message)
+    log(message)
 
 
 @bot.message_handler(commands=['pidor'])
 def pidor_command(message):
     pidor.choose_winner(message)
+    log(message)
 
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
     about_and_help.my_help(message)
+    log(message)
 
 
 @bot.message_handler(commands=['about', 'start'])
 def about_command(message):
     about_and_help.about(message)
+    log(message)
 
 
 @bot.message_handler(commands=['kek'])
 def kek(message):
     bot.send_message(message.chat.id, config.kek_message, reply_to_message_id=message.message_id)
+    log(message)
 
 
 @bot.message_handler(content_types=['text'])
@@ -62,6 +72,8 @@ def bl_message(message):
     if re.search(config.fuck_u_from_detur_pattern, message.text) and (message.from_user.username == 'MaliciousMoon' or
                                                   message.from_user.first_name == 'Detur'):
         bot.reply_to(message, config.fuck_u_detur)
+
+    log(message)
 
 
 @bot.inline_handler(func=lambda query: len(query.query.split()) > 1)
