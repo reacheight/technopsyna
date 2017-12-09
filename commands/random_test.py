07d284180900e487c2f7bc9b6314b0e72c53740b
reacheight <reacheight8@gmail.com>
2017-12-09 18:09:00 +0300

    fixed another bugs

--- commands/random_test.py
+++ commands/random_test.py
@@ -14,8 +14,8 @@ def get_random_test(subject):
 
 def random_test(message):
     splited = message.text.split()
-    if len(message.text.split()) < 2 or splited[1] not in config.test_files:
+    if len(splited) < 2 or splited[1] not in config.test_files:
         bot.reply_to(message, config.random_test_error_subject, parse_mode='Markdown')
         return
 
-    bot.reply_to(message, get_random_test(splited[1]))
+    bot.reply_to(message, get_random_test(splited[1]), disable_web_page_preview=True)
