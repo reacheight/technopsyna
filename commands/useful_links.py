5599a850ecc314454154a26bab8fed0e70b496b5
reacheight <reacheight8@gmail.com>
2017-12-10 22:18:48 +0300

    fixed markdown-bug

--- commands/useful_links.py
+++ commands/useful_links.py
@@ -4,4 +4,4 @@ from bot import bot
 
 def links(message):
     with open(config.links_list, "r") as f:
-        bot.reply_to(message.chat.id, f.read(), parse_mode="Markdown", disable_web_page_preview=True)
+        bot.reply_to(message, f.read(), parse_mode="Markdown", disable_web_page_preview=True)
