9d4b470c11ab92e395dcacb4c012e85c7aba7cc5
reacheight <reacheight8@gmail.com>
2018-02-07 15:51:29 +0300

    logs disabled

--- techconfbot.py
+++ techconfbot.py
@@ -2,7 +2,6 @@ import re
 
 from commands import bl, about_and_help, wolfram, tts, deer, random_test, useful_links, number_fact, dembel_countdown
 from bot import bot
-from log import log
 
 import config
 
@@ -10,66 +9,55 @@ import config
 @bot.message_handler(commands=['random_test'])
 def random_test_command(message):
     random_test.random_test(message)
-    log(message)
 
 
 @bot.message_handler(commands=['bl'])
 def bl_command(message):
     bl.my_bl(message)
-    log(message)
 
 
 @bot.message_handler(commands=['wf', 'wolfram'])
 def wf_command(message):
     wolfram.wolfram_command(message)
-    log(message)
 
 
 @bot.message_handler(commands=['tts', 'voice'])
 def tts_command(message):
     tts.text_to_speech(message)
-    log(message)
 
 
 @bot.message_handler(commands=['dembel'])
 def dembel_command(message):
     dembel_countdown.dmb_days(message)
-    log(message)
 
 
 @bot.message_handler(commands=['number_fact'])
 def number_fact_command(message):
     number_fact.type_number_fact(message)
-    log(message)
 
 
 @bot.message_handler(commands=['deer_message'])
 def deer_message_command(message):
     deer.get_messages(message)
-    log(message)
 
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
     about_and_help.my_help(message)
-    log(message)
 
 
 @bot.message_handler(commands=['about', 'start'])
 def about_command(message):
     about_and_help.about(message)
-    log(message)
 
 @bot.message_handler(commands=['links'])
 def useful_links_command(message):
     useful_links.links(message)
-    log(message)
 
 
 @bot.message_handler(commands=['kek'])
 def kek(message):
     bot.send_message(message.chat.id, config.kek_message, reply_to_message_id=message.message_id)
-    log(message)
 
 
 @bot.message_handler(content_types=['text'])
@@ -79,8 +67,6 @@ def bl_message(message):
 
     if re.search(config.chto_pacani_pattern, message.text):
         bot.send_sticker(message.chat.id, config.cho_pacani_anime_sticker, reply_to_message_id=message.message_id)
-
-    log(message)
 
 
 @bot.message_handler(content_types=['new_chat_members'])
