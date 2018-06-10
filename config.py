4678b333fd45d5a1a1348c19d704677e2b055a68
reacheight <reacheight8@gmail.com>
2018-06-10 10:37:52 +0300

    deleted unrelated config data

--- config.py
+++ config.py
@@ -6,40 +6,20 @@ about_text_file = 'data/about_data/about_text_message'
 help_text_file = 'data/help_data/help_text_message'
 bl_text_file = 'data/bl_data/bl_text_messages'
 bl_images_locations = 'data/bl_data/images/'
-deer_messages_location = 'data/deer_data/messages'
-math_test_file = 'data/tests/math'
 links_list = 'data/links_list'
 passing_scores_file = 'data/passing_scores'
-test_files = {'math' : 'data/tests/math',
-              'rus' : 'data/tests/rus',
-              'phys' : 'data/tests/phys',
-              'inf' : 'data/tests/inf'}
 
 wolfram_appid = os.environ['WOLFRAM_APPID']
 wolfram_bad_status_message = "Запрос не найдён.\nЕсли ты ввёл его на русском, то попробуй ввести его на английском."
 wolfram_empty_query_message = "Использование: `/wolfram <запрос>` или `/wf <запрос>`"
 
-tts_empty_query_message = "Использование: `/voice <запрос>` или `/tts <запрос>`"
-
-random_test_error_subject = "Использование: /random\_test `math` или `inf` или `rus` или `phys`"
-
-number_fact_empty = "Использование: /number\_fact `число`"
-
 my_id = os.environ['MY_ID']
-
-kek_message = "Вы ботом ошиблись.."
 
 chto_sdaesh_sticker = 'CAADAgADrQEAAm29TQUoveU--qPBlAI'
 cho_pacani_anime_sticker = 'CAADAgADJwADtIuIDaIy4m-uZXREAg'
 chto_pacani_pattern = r'(?iu).*чт?[оеё],? п[ао][цс][ао]ны'
 integer_pattern = r'[\d]+'
 
-number_api_url = 'http://numbersapi.com/'
-
 deer_dembel_date = {'year' : 2018, 'month' : 12, 'day' : 13}
 
 technoconfa = 'Техноконфа_2018'
-
-post_excuses = 'data/excuses/post_excuses'
-pre_excuses = 'data/excuses/pre_excuses'
-excuse_empty_message = "Использование: /excuse `<что вы хотите оправдать>`"
