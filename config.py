7045fe36151120ba52e0948cc6074f502befe9e6
reacheight <reacheight8@gmail.com>
2018-07-07 23:02:04 +0300

    chat name updated

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ chto_pacani_pattern = r'(?iu).*чт?[оеё],? п[ао][цс][ао]ны'
 
 deer_dembel_date = {'year': 2018, 'month': 12, 'day': 13}
 
-technoconfa_chatname = 'Техноконфа_2018'
+technoconfa_chatname = 'Техноконфа_2019'
 bot_username = '@technoconfachbot'
 
 logs_channel = '@technocofachbot_logs'
