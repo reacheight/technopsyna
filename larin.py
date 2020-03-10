3883c612776195a9dea8177104f70975c8bfa257
Roma Mendaliev <reacheight8@gmail.com>
2020-03-10 14:19:12 +0300

    Decode redis byte data to string

--- larin.py
+++ larin.py
@@ -5,5 +5,5 @@ db = redis.from_url(config.redis_url)
 
 
 def get_latest_var_url():
-    var_number = db.get(config.larin_var_key)
+    var_number = db.get(config.larin_var_key).decode('utf-8')
     return config.larin_variant_pdf_template.format(var_number)
