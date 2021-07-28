a9a67c7ecda8ebd4d93bbf6ad7110d9fa477be68
Nikita <31010564+y0f0@users.noreply.github.com>
2021-07-28 10:15:46 +0300

    Update technopsyna/techconfbot.py
    Co-authored-by: Roman Tchaikovsky <reacheight8@gmail.com>

--- technopsyna/techconfbot.py
+++ technopsyna/techconfbot.py
@@ -124,7 +124,7 @@ async def pidoroconfa_new_member_check(message: types.Message):
     )
 
     await bot.send_message(
-        message.chat.id, f'Привет, {username}, добро пожаловать в пидорконфу. Нажмите на кнопку ниже, '
+        message.chat.id, f'Привет, {username}, добро пожаловать в пидороконфу. Нажмите на кнопку ниже, '
                          f'чтобы подтвердить, '
                          f'что вы человек. Если вы не сделаете этого в течение n минут, вас автоматически исключат из '
                          f'группы.',
